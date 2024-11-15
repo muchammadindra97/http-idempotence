b7849c96a4f30085180f03eee310d3e44423f4c8
Muchammad Indra <muchammadindra97@gmail.com>
2024-11-15 21:34:09 +0700

    feat(backend): database setup and init function

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import services.database
+import services.banking
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE='./flaskr.sqlite',
+    )
+
+    services.database.register_db(app)
+
+    @app.post('/init-db')
+    def init_db():
+        services.database.init_db()
+        return jsonify({
+            'message': 'Database initialized'
+        })
+
+    # app.register_blueprint(services.banking.bp)
+
+    return app
---
+++ backend/services/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+from flask import g, current_app, Flask
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db(e = None):
+    db: sqlite3.Connection = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def register_db(app: Flask):
+    app.teardown_appcontext(close_db)
+
+def init_db():
+    db = get_db()
+    db.execute("CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY, amount INTEGER, action TEXT, time TIMESTAMP)")
+    db.execute("CREATE TABLE IF NOT EXISTS idempotence_key(id INTEGER PRIMARY KEY, key TEXT, time TIMESTAMP)")
